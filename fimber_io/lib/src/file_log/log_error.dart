eb2c8ac592f86ffc6cfffd76dff9674942a15fdd
yu840915 <yu840915@gmail.com>
2022-12-08 11:00:11 +0800

    Format context

--- fimber_io/lib/src/file_log/log_error.dart
+++ fimber_io/lib/src/file_log/log_error.dart
@@ -7,7 +7,22 @@ class LogError {
   final StackTrace? stackTrace;
   final Map<String, dynamic>? context;
 
-  LogError({
+  factory LogError({
+    required String message,
+    required dynamic error,
+    required String tag,
+    StackTrace? stackTrace,
+    Map<String, dynamic>? context,
+  }) =>
+      LogError._(
+        message: message,
+        error: error,
+        tag: tag,
+        stackTrace: stackTrace,
+        context: context != null ? jsonifyContext(context) : null,
+      );
+
+  LogError._({
     required this.message,
     required this.error,
     required this.tag,
