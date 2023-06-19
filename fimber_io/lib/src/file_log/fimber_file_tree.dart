640af5e035cc07eced8f78c518f156a2408b478b
Jimmy Tai <jimmytai0315@hotmail.com>
2023-06-19 15:27:53 +0800

    Update `fimber_io` package to support dart3.

--- fimber_io/lib/src/file_log/fimber_file_tree.dart
+++ fimber_io/lib/src/file_log/fimber_file_tree.dart
@@ -3,13 +3,6 @@ import 'package:fimber/fimber.dart';
 import 'file_tree.dart';
 
 class FimberFileTree extends FileTree {
-  factory FimberFileTree.elapsed(String fileName,
-      {List<String> logLevels = CustomFormatTree.defaultLevels}) {
-    return FimberFileTree(fileName,
-        logFormat: '${CustomFormatTree.timeElapsedToken}'
-            '\t${CustomFormatTree.messageToken}');
-  }
-
   FimberFileTree(
     String fileName, {
     String logFormat =
@@ -17,11 +10,25 @@ class FimberFileTree extends FileTree {
     List<String> logLevels = CustomFormatTree.defaultLevels,
     int maxBufferSize = FileTree.defaultBufferSizeLimit,
     int bufferWriteInterval = FileTree.defaultBufferFlushInterval,
-  }) : super(fileName,
-            logLevels: logLevels,
-            logFormat: logFormat,
-            maxBufferSize: maxBufferSize,
-            bufferWriteInterval: bufferWriteInterval);
+  }) : super(
+          fileName,
+          logLevels: logLevels,
+          logFormat: logFormat,
+          maxBufferSize: maxBufferSize,
+          bufferWriteInterval: bufferWriteInterval,
+        );
+
+  factory FimberFileTree.elapsed(
+    String fileName, {
+    List<String> logLevels = CustomFormatTree.defaultLevels,
+  }) {
+    return FimberFileTree(
+      fileName,
+      logFormat: '${CustomFormatTree.timeElapsedToken}'
+          '\t${CustomFormatTree.messageToken}',
+      logLevels: logLevels,
+    );
+  }
 
   @override
   void onBufferFlushed(String fileName, int fileSize) {}
